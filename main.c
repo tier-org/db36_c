c3f841b14e12b99ecde037c7e33ff1cf3e0112c7
Eugene Khashin <adastreamer@gmail.com>
2023-02-24 03:17:10 +0300

    posix fallocate

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 #include <string.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 #define one 1
 #define pathDelim '/'
 #define strTerm '\0'
+
+#define DB36_C_OK 0
+#define DB36_C_BLB_ENOENT 166
 
 struct blob {
     //
@@ -21,6 +25,8 @@ struct blob {
     // calc values are not set directly, just using a constructor
     //
     FILE *file;
+    int fd;
+    struct stat *filestat;
     unsigned short keyBitsSize; // number of bits of a key
     unsigned short recordSize; // size of one record: valueSize or keySize + valueSize
     unsigned long long recordsCount; // count of records
@@ -28,7 +34,7 @@ struct blob {
     unsigned short shift;
 };
 
-void init_params(struct blob *bl) {
+int init_params(struct blob *bl) {
     bl->keyBitsSize = bl->keySize;
     bl->keyBitsSize = bl->keyBitsSize << 3;
     bl->recordsCount = one; // init value to shift it
@@ -43,49 +49,47 @@ void init_params(struct blob *bl) {
         bl->recordSize = bl->valueSize;
     }
     bl->capacitySize = bl->recordSize * bl->recordsCount;
+    return DB36_C_OK;
 }
 
-void init_file(struct blob *bl) {
+int init_file(struct blob *bl) {
     bl->dir = strdup(bl->path);
     bl->dir[strrchr(bl->path, pathDelim) - bl->path] = strTerm;
-    
-    
 
+    bl->fd = open(bl->path, O_RDWR|O_CREAT, 0666);
+    if (bl->fd < 0) {
+        return DB36_C_BLB_ENOENT;
+    }
+
+    posix_fallocate(bl->fd, 0, bl->capacitySize);
+    bl->file = fdopen(bl->fd, "w");
+
+    return DB36_C_OK;
 }
 
-void init(struct blob *bl) {
-    init_params(bl);
-    init_file(bl);
-}
 
 int main() {
+    int s = DB36_C_OK;
     struct blob *b1 = calloc(one, sizeof *b1);
 
-    b1->path = strdup("./data/tests/test.bl");
+    b1->path = strdup("/db36_c/data/tests/test.bl");
     b1->keySize = 4;
     b1->valueSize = 4;
     b1->capacity = 16;
 
+    if ((s = init_params(b1)) != DB36_C_OK) {
+        return s;
+    }
+    if ((s = init_file(b1)) != DB36_C_OK) {
+        return s;
+    }
+
     printf("%s\n", b1->path);
     printf("%s\n", b1->dir);
-
     printf("%u\n", b1->keySize);
     printf("%u\n", b1->shift);
     printf("%lld\n", b1->recordsCount);
     printf("%d\n", b1->recordSize);
-
-    printf("-\n");
-    init(b1);
-
-    printf("%s\n", b1->path);
-    printf("%s\n", b1->dir);
-
-    printf("%u\n", b1->keySize);
-    printf("%u\n", b1->shift);
-    printf("%lld\n", b1->recordsCount);
-    printf("%d\n", b1->recordSize);
-
-    // (b1 -> shift)++;
 
     return 0;
 }
